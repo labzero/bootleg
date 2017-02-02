273310e76c54ca6cf464e8ccec83adefbc47bd03
Brien Wankel <brien@reebosak.net>
2017-02-02 13:29:12 -0700

    Add deploy task placeholder

---
+++ lib/mix/tasks/deploy.ex
@@ -0,0 +1,18 @@
+defmodule Mix.Tasks.Bootleg.Deploy do
+  use Mix.Task
+
+  @shortdoc "Deploy a release from the local store"
+
+  @moduledoc """
+  Build a release
+
+  # Usage:
+
+    * mix bootleg.deploy [env] [release] [Options]
+
+  """
+  @spec run(OptionParser.argv) :: :ok
+  def run(args) do
+
+  end
+end
