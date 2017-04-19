0e845f9c17d5697d1e50857651753ca9d288842f
Edward Holets <ned@labzero.com>
2017-04-19 15:51:45 -0700

    Pass our SSH identity configuration to git.

--- lib/git.ex
+++ lib/git.ex
@@ -2,16 +2,16 @@ defmodule Bootleg.Git do
   
   alias Bootleg.Shell
 
-  def remote(args) do
-    git("remote", args)
+  def remote(args, options \\ []) do
+    git("remote", args, options)
   end
 
-  def push(args) do
-    git("push", args)
+  def push(args, options \\ []) do
+    git("push", args, options)
   end
     
-  defp git(cmd, args) do
-    Shell.run("git", [cmd | args])
+  defp git(cmd, args, options \\ []) do
+    Shell.run("git", [cmd | args], options)
   end
   
 end
