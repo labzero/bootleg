4fcecf70b87659b718f2fdc59ec9474b6f98fff0
Sasha Voynow <sasha@labzero.com>
2017-04-19 19:17:40 -0700

    move System.cmd usages to their own module

---
+++ lib/shell.ex
@@ -0,0 +1,15 @@
+defmodule Bootleg.Shell do
+
+  def run(cmd, args, opts \\ []) do
+    System.cmd(cmd, args, set_defaults(opts))
+  end
+
+  defp set_defaults(opts) do
+    Keyword.merge(opts, default_opts)
+  end
+
+  defp default_opts, do: [stderr_to_stdout: true]
+end
+
+
+
