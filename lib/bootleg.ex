16039c4d22911231abaed2a137604d8c165e554b
Edward Holets <ned@labzero.com>
2017-04-26 13:24:45 -0700

    Add configuration documentation.

--- lib/bootleg.ex
+++ lib/bootleg.ex
@@ -1,15 +1,52 @@
 defmodule Bootleg do
 
   @moduledoc """
-  Documentation for Bootleg.
+  Makes building and deploying your app easier then getting a prohibition-era drink.
+
+  Integrating `Bootleg` into your application is a matter of setting up an appropriate configuration. See
+  `Bootleg.Config` for details on configuration.
   """
 
   alias Mix.Project
   
   defmodule BuildConfig do
+    @moduledoc """
+    Configuration for the build tasks.
 
+    The actual configuration values are set via the `Mix.Config` for
+    the target project, as a `Map` under the `:bootleg` application.
+
+    ## Fields
+    * `strategy` - The bootleg strategy to use for builds. Defaults to `Bootleg.Strategies.Build.RemoteSSH`.
+    * `workspace` - Absolute path to a directory on the build host where the build will occur. This directory
+        will be created if its not already.
+    * `revision` - The revision to build.
+    * `user` - The username to use when connecting to the build host.
+    * `host` - The hostname or IP of the build host.
+    * `mix_env` - What `MIX_ENV` to use for the build.
+    * `identity` - Absolute path to a private key used to authenticate with the build host. This should be in `PEM` format.
+
+    ## Example
+
+      ```
+      config :bootleg, build: [
+        strategy: Bootleg.Strategies.Build.RemoteSSH,
+        host: "build1.example.com",
+        user: "jane",
+        workspace: "/usr/local/my_app/build"
+      ]
+      ```
+    """
+
+    @doc false
     defstruct [:identity, :host, :mix_env, :revision, :strategy, :user, :workspace]
 
+    @doc """
+    Creates a `Bootleg.BuildConfig`.
+
+    The keys in the map should match the fields in the struct.
+    """
+    @spec init(map) :: %Bootleg.BuildConfig{}
     def init(config) do
       %__MODULE__{
         identity: config[:identity],
@@ -25,9 +62,38 @@ defmodule Bootleg do
   end
 
   defmodule DeployConfig do
+    @moduledoc """
+    Configuration for the deploy tasks.
 
+    ## Fields
+      * `workspace` - Absolute path to the directory where the deploy should be placed on the deployment host. This directory
+          will be created if its not already.
+      * `strategy` - The bootleg strategy to use for deployments. Defaults to `Bootleg.Strategies.Deploy.RemoteSSH`.
+      * `user` - The username to use when connecting to the deployment host.
+      * `host` - The hostname or IP of the deployment host.
+      * `identity` - Absolute path to a private key used to authenticate with the deployment host. This should be in `PEM` format.
+
+    ## Example
+
+      ```
+      config :bootleg, deploy: [
+        strategy: Bootleg.Strategies.Deploy.RemoteSSH,
+        host: "build1.example.com",
+        user: "jane",
+        workspace: "/usr/local/my_app/release"
+      ]
+      ```
+    """
+
+    @doc false
     defstruct [:workspace, :identity, :host, :strategy, :user]
 
+    @doc """
+    Creates a `Bootleg.DeployConfig` struct.
+
+    The keys in the `Map` should match the fields in the struct.
+    """
+    @spec init(map) :: %Bootleg.DeployConfig{}
     def init(config) do
       %__MODULE__{
         workspace: config[:workspace],
@@ -41,19 +107,65 @@ defmodule Bootleg do
   end
 
   defmodule Config do
+    @moduledoc """
+    Configuration for bootleg in general.
 
-    defstruct [:app, :version, :build, :deploy, :archive]
+    The configuration is defined as a `Map` in the `Mix.Config` of the target project,
+    under the key `:bootleg`. Attributes in the struct have a 1:1 relationship with
+    the keys in the `Mix.Config`.
 
+    ## Fields
+    * `app` - The name of the app being managed by `Bootleg`.
+    * `version` - The version of the app. Defaults to the `Mix.Project` version.
+    * `build` - Configuration for the build tasks. This should be a `Map` in `Mix.Config`, and will 
+        be converted to a `Bootleg.BuildConfig` using `Bootleg.BuildConfig.init/1`.
+    * `deploy` - Configuration for the deployment tasks. This should be a `Map` in `Mix.Config`, and will 
+        be converted to a `Bootleg.DeployConfig` using `Bootleg.DeployConfig.init/1`.
+    * `push_options` - Any extra options to use for `git push`, defaults to `-f` (force push).
+    * `refspec` - Which git [refspec](https://git-scm.com/book/id/v2/Git-Internals-The-Refspec) to use when pushing, defaults to `master`.
+
+    ## Example
+
+      ```
+      config :bootlet, app: "my_app"
+      config :bootleg, build: [
+        strategy: Bootleg.Strategies.Build.RemoteSSH,
+        host: "build1.example.com",
+        user: "jane",
+        workspace: "/usr/local/my_app/build"
+      ]
+      config :bootleg, deploy: [
+        strategy: Bootleg.Strategies.Deploy.RemoteSSH,
+        host: "build1.example.com",
+        user: "jane",
+        workspace: "/usr/local/my_app/release"
+      ]
+      ```
+    """
+
+    @doc false
+    defstruct [:app, :version, :build, :deploy, :archive, :push_options, :refspec]
+
+    @doc """
+    Creates a `Bootleg.Config` from the `Application` configuration (under the key `:bootleg`).
+
+    The keys in the map should match the fields in the struct.
+    """
+    @spec init :: %Bootleg.Config{build: %Bootleg.BuildConfig{}, deploy: %Bootleg.DeployConfig{}}
     def init do
       %__MODULE__{
         app: Application.get_env(:bootleg, :app),
         version: Project.config[:version],            
         build: Bootleg.BuildConfig.init(Application.get_env(:bootleg, :build)),
-        deploy: Bootleg.DeployConfig.init(Application.get_env(:bootleg, :deploy))
+        deploy: Bootleg.DeployConfig.init(Application.get_env(:bootleg, :deploy)),
+        push_options: Application.get_env(:bootleg, :push_options),
+        refspec: Application.get_env(:bootleg, :refspec),
       }
     end
   end
 
+  @doc "Alias for `Bootleg.Config.init/0`."
+  @spec config :: %Bootleg.Config{}
   def config do
     Bootleg.Config.init()
   end
