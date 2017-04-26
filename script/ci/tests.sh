30da0d72702441214a58bcca4038bb70ca5e3816
Edward Holets <ned@labzero.com>
2017-04-26 14:52:53 -0700

    Require dialyzer and credo to pass during CI.

--- script/ci/tests.sh
+++ script/ci/tests.sh
@@ -3,5 +3,13 @@
 export MIX_ENV="test"
 export PATH="$HOME/dependencies/erlang/bin:$HOME/dependencies/elixir/bin:$PATH"
 mix dialyzer
+DIALYZER=$?
 mix credo
+CREDO=$?
 mix test
+TEST=$?
+
+if [ $DIALYZER -ne 0 ] || [ $CREDO -ne 0 ] || [ $TEST -ne 0 ]
+then
+  exit 1
+fi
