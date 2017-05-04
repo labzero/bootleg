d54b5dacdaaee463415456cddf03af7217f2f2e1
Rob Adams <pillage@gmail.com>
2017-05-04 10:02:06 -0700

    Add Administration Ping command (#37)

--- test/strategies/administration/remote_ssh_test.exs
+++ test/strategies/administration/remote_ssh_test.exs
@@ -9,7 +9,7 @@ defmodule Bootleg.Strategies.Administration.RemoteSSHTest do
       config:
         %Bootleg.Config{
           app: "bootleg",
-          version: "1",
+          version: "1.0.0",
           administration:
             %Bootleg.AdministrationConfig{
               identity: "identity",
@@ -17,14 +17,31 @@ defmodule Bootleg.Strategies.Administration.RemoteSSHTest do
               host: "host",
               user: "user"
             }
-        }
+        },
+      bad_config:
+        %Bootleg.Config{
+          app: "Funky Monkey",
+          version: "1.0.0",
+          administration:
+            %Bootleg.AdministrationConfig{
+              identity: nil,
+              "workspace": "what",
+              host: nil
+            }
+          }
     }
   end
 
-  test "init", %{config: config} do
+  test "init good", %{config: config} do
     RemoteSSH.init(config)
     assert_received({Bootleg.SSH, :start})
     assert_received({Bootleg.SSH, :connect, ["host", "user", [identity: "identity", workspace: "."]]})
+  end
+
+  test "init bad", %{bad_config: config} do
+    assert_raise RuntimeError, ~r/This strategy requires "host", "user" to be configured/, fn ->
+      RemoteSSH.init(config)
+    end
   end
 
   test "start", %{config: %{app: app} = config} do
@@ -40,5 +57,10 @@ defmodule Bootleg.Strategies.Administration.RemoteSSHTest do
   test "restart", %{config: %{app: app} = config} do
     RemoteSSH.restart(:conn, config)
     assert_received({Bootleg.SSH, :"run!", [:conn, "bin/bootleg restart"]})
+  end
+
+  test "ping", %{config: %{app: app} = config} do
+    RemoteSSH.ping(:conn, config)
+    assert_received({Bootleg.SSH, :"run!", [:conn, "bin/bootleg ping"]})
   end
 end
