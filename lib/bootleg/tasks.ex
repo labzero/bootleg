02112d8fabdf460d81ab17d63618c3e575d14b7c
Brien Wankel <brien@reebosak.net>
2017-07-10 16:06:14 -0700

    Load user config after loading internal tasks (#93)

--- lib/bootleg/tasks.ex
+++ lib/bootleg/tasks.ex
@@ -7,8 +7,6 @@ defmodule Bootleg.Tasks do
   def load_tasks do
     use Config
 
-    Config.load("config/deploy.exs")
-
     tasks_path = Path.join(__DIR__, "tasks")
     parent_env = %{__ENV__ | line: 1}
 
@@ -17,5 +15,8 @@ defmodule Bootleg.Tasks do
     |> Enum.map(fn (file) -> Path.join(tasks_path, file) end)
     |> Enum.map(fn (file) -> {File.read!(file), %{parent_env | file: file}} end)
     |> Enum.each(fn ({code, env}) -> Code.eval_string(code, [], env) end)
+
+    Config.load("config/deploy.exs")
+    :ok
   end
 end
