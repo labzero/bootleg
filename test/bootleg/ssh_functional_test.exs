14ffb43e0886dbc8ca78ab4f596852a736665151
Edward Holets <ned@labzero.com>
2017-07-12 13:29:21 -0700

    Add SSH functional tests for identity.

--- test/bootleg/ssh_functional_test.exs
+++ test/bootleg/ssh_functional_test.exs
@@ -1,6 +1,6 @@
 defmodule Bootleg.SSHFunctionalTest do
   use Bootleg.FunctionalCase, async: false
-  alias Bootleg.{Host, Role, SSH, Fixtures}
+  alias Bootleg.{Host, Role, SSH}
   alias SSHKit.Context, as: SSHKitContext
   alias SSHKit.Host, as: SSHKitHost
   import ExUnit.CaptureIO
@@ -11,7 +11,7 @@ defmodule Bootleg.SSHFunctionalTest do
         hosts: Enum.map(hosts, &Host.init(&1.ip, docker_ssh_opts(&1), [])),
         name: :build,
         user: "blammo",
-        options: [workspace: "some workspace"]
+        options: [workspace: "some_workspace"]
       }
     }
   end
@@ -42,12 +42,14 @@ defmodule Bootleg.SSHFunctionalTest do
   end
 
   @tag boot: 2
-  test "init/2 with Bootleg.Role", %{role: role} do
+  test "init/2 with Bootleg.Role", %{role: role, hosts: hosts} do
+    ip_1 = List.first(hosts).ip
+    ip_2 = List.last(hosts).ip
     capture_io(fn ->
       assert %SSHKitContext{hosts: [
-        %SSHKitHost{name: "127.0.0.1", options: options_1},
-        %SSHKitHost{name: "127.0.0.1", options: options_2}
-      ], path: "some workspace"} = SSH.init(role), "Connection isn't a context"
+        %SSHKitHost{name: ^ip_1, options: options_1},
+        %SSHKitHost{name: ^ip_2, options: options_2}
+      ], path: "some_workspace"} = SSH.init(role), "Connection isn't a context"
       assert options_1[:port] != options_2[:port]
       assert options_1[:user] ==  "me"
     end)
@@ -56,30 +58,33 @@ defmodule Bootleg.SSHFunctionalTest do
   @tag boot: 1
   test "init/2 with Role name atom", %{hosts: [host]} do
     use Bootleg.Config
+    ip = host.ip
     role :build, host.ip, port: host.port, user: host.user, password: host.password,
       workspace: "/tmp/foo", silently_accept_hosts: true
 
     capture_io(fn ->
       assert %SSHKitContext{
-        hosts: [%SSHKitHost{name: "127.0.0.1", options: options}],
+        hosts: [%SSHKitHost{name: ^ip, options: options}],
         path: "/tmp/foo"} = SSH.init(:build)
       assert options[:user] == host.user
     end)
   end
 
-  @tag skip: "identity file needs to be added to docker"
+  @tag boot: 1
   test "init/2 with Role name atom and identity", %{hosts: [host]} do
-    use Bootleg.{Config}
-    role :build, host.ip, port: host.port, user: host.user,
-      workspace: "/", silently_accept_hosts: true, identity: Fixtures.identity_path
+    # credo:disable-for-next-line Credo.Check.Consistency.MultiAliasImportRequireUse
+    use Bootleg.Config
+    ip = host.ip
+    role :build, ip, port: host.port, user: host.user,
+      workspace: "/", silently_accept_hosts: true, identity: host.private_key_path
 
     capture_io(fn ->
       assert %SSHKitContext{
-        hosts: [%SSHKitHost{name: "build.labzero.com", options: options}],
-        path: "some path"} = SSH.init(:build)
+        hosts: [%SSHKitHost{name: ^ip, options: options}],
+        path: "/"} = SSH.init(:build)
       assert options[:user] == host.user
-      assert options[:identity] == Fixtures.identity_path
-      assert {SSHKit.SSH.ClientKeyAPI, _} = options[:key_cb]
+      assert {SSHClientKeyAPI, key_details} = options[:key_cb]
+      assert key_details[:identity_data] == host.private_key
     end)
   end
 
@@ -88,6 +93,37 @@ defmodule Bootleg.SSHFunctionalTest do
     capture_io(fn ->
       conn = SSHKit.context(SSHKit.host(host.ip))
       assert_raise SSHError, fn -> SSH.run!(conn, "echo foo") end
+    end)
+  end
+
+  @tag boot: 2
+  test "run!", %{role: role, hosts: [host_1, host_2]} do
+    ip_1 = host_1.ip
+    ip_2 = host_2.ip
+    capture_io(fn ->
+      conn = SSH.init(role)
+      assert [{:ok, [stdout: "hello\n"], 0, %{name: ^ip_1}},
+            {:ok, [stdout: "hello\n"], 0, %{name: ^ip_2}}] = SSH.run!(conn, "echo hello")
+    end)
+  end
+
+  @tag boot: 1
+  test "upload", %{role: role} do
+    capture_io(fn ->
+      conn = SSH.init(role)
+      assert :ok == SSH.upload(conn, "test/fixtures/build.tar.gz", "new_remote_file")
+      assert [{:ok, _, 0, _}] = SSH.run!(conn, "ls -la new_remote_file")
+    end)
+  end
+
+  @tag boot: 1
+  test "download", %{role: role} do
+    capture_io(fn ->
+      path = Temp.path!("scp-download")
+      conn = SSH.init(role)
+      assert :ok == SSH.download(conn, "/etc/hosts", path)
+      assert File.regular?(path)
+      File.rm!(path)
     end)
   end
 end
