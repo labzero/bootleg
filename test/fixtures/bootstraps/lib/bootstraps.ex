64b2b4f5f6e48ef5c8df0ac660f8b7ec7ac558b8
Edward Holets <ned@labzero.com>
2017-07-13 14:22:22 -0700

    Add end-to-end functional test.

---
+++ test/fixtures/bootstraps/lib/bootstraps.ex
@@ -0,0 +1,18 @@
+defmodule Bootstraps do
+  @moduledoc """
+  Documentation for Bootstraps.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bootstraps.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
