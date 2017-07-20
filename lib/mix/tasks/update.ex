3d7108666893ab94b43aa31f07e8cc4d302583e1
Brien Wankel <brien@reebosak.net>
2017-07-20 14:54:57 -0700

    Add `mix bootleg.update` command

---
+++ lib/mix/tasks/update.ex
@@ -0,0 +1,14 @@
+defmodule Mix.Tasks.Bootleg.Update do
+  use Bootleg.Task, :update
+
+  @shortdoc "Build, deploy, and start a release all in one command."
+
+  @moduledoc """
+  Update a release
+
+  # Usage:
+
+    * mix bootleg.update
+  """
+
+end
