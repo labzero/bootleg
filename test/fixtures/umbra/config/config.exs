3e32a6a8d9fa7239e70323576735560e45e4d195
Andrew Dixon <dixon@largearcade.com>
2026-05-22 16:44:11 -0700

    Silence Config Deprication Warning

--- test/fixtures/umbra/config/config.exs
+++ test/fixtures/umbra/config/config.exs
@@ -1,13 +1,13 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 # By default, the umbrella project as well as each child
 # application will require this configuration file, ensuring
 # they all use the same configuration. While one could
 # configure all applications here, we prefer to delegate
 # back to each application for organization purposes.
-import_config "../apps/*/config/config.exs"
+for config <- Path.wildcard(Path.expand("../apps/*/config/config.exs", __DIR__)) do
+  import_config config
+end
 
 # Sample configuration (overrides the imported configuration above):
 #
