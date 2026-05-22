3cf609661e0b623fe3a1115a1211fbe3c17580ae
Andrew Dixon <dixon@largearcade.com>
2026-05-22 16:09:28 -0700

    Quiet Debug Messages
    Set log loven to warning for tests.

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,5 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Config module.
 import Config
+
+if config_env() == :test, do: import_config("test.exs")
