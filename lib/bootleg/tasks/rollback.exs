5cc47aea3970e912cb9598d7da72c71ac020cf58
Andrew Dixon <dixon@largearcade.com>
2026-06-01 12:09:09 -0700

    Add Rollback Tests
    Debug a bad rollback. Who knew tests were so useful?

--- lib/bootleg/tasks/rollback.exs
+++ lib/bootleg/tasks/rollback.exs
@@ -6,9 +6,8 @@ task :rollback do
 
   remote :app do
     "test $(ls -1d releases/*/ 2>/dev/null | wc -l) -ge 2 || (echo 'No previous release to roll back to' && exit 1)"
-    "previous=$(ls -1dt releases/*/ | sed -n '2p') && ln -sfn $previous current"
-    "current/bin/#{app} restart"
+    "ls -1dt releases/*/ | tail -2 | head -1 | xargs -I{} ln -sfn {} current"
   end
 
-  UI.info("#{app} rolled back")
+  UI.info("⏪ #{app} rolled back")
 end
---
+++ test/bootleg/tasks/rollback_task_functional_test.exs
@@ -0,0 +1,52 @@
+defmodule Bootleg.Tasks.RollbackTaskFunctionalTest do
+  use Bootleg.FunctionalCase, async: false
+  use Bootleg.DSL
+  import ExUnit.CaptureIO
+
+  setup %{hosts: [host], role_opts: role_opts} do
+    role(
+      :app,
+      [host.ip],
+      port: host.port,
+      user: host.user,
+      password: host.password,
+      silently_accept_hosts: true,
+      workspace: "workspace",
+      release_workspace: role_opts[:release_workspace]
+    )
+
+    config :app, "my_app"
+    config :version, "valid_archive"
+  end
+
+  test "rollback/1 returns an error if there is no release to rollback to" do
+    File.cd!("test/fixtures", fn ->
+      capture_io(fn ->
+        remote :app do
+          "mkdir -p releases/2/"
+          "ln -s releases/2/ current"
+        end
+        assert_raise SSHError, fn -> invoke(:rollback) end
+      end)
+    end)
+  end
+
+  test "rollback/1 rollsback the release" do
+    File.cd!("test/fixtures", fn ->
+      capture_io(fn ->
+        remote :app do
+          "mkdir -p releases/1/"
+          "mkdir -p releases/2/"
+          "ln -s releases/2/ current"
+        end
+        assert [{:ok, [stdout: "releases/2/"], 0, _}] =
+          remote(:app, "ls -lh current | sed 's/.* //' | tr -d '\n'")
+        invoke(:rollback)
+        assert [{:ok, [stdout: "releases/1/"], 0, _}] =
+          remote(:app, "ls -lh current | sed 's/.* //' | tr -d '\n'")
+      end)
+    end)
+  end
+
+  test "rollback/1 removes the rolled back release"
+end
